9d7d8ba0feabc1cc461af5d6eafc82de7cc6bc63
George Garcia <geor@vicki-2.local>
2018-03-20 21:45:47 -0700

    Just practicing more swift syntax. Made a simple BMI Calculator.

--- BMI Calc/main.swift
+++ BMI Calc/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func calculateBMI(weight: Double, height: Double) -> Double {
+    
+    var total = 0.0
+    total = (Double(weight * 703)) / (Double(height * height))
+    
+    if(total > 25.0){
+        print("Time to go hard in the gym! You are overweight!")
+    } else if(total > 18.50 || total < 25.0){
+        print("Not bad! You are at normal weight!")
+    } else if(total < 18.50){
+        print("Woah! You need some meat in ya! You are underweight!!")
+    }
+    
+    return total
+    
+}
 
+print("Hello user! Let me help you calculate your BMI!")
+print("Enter your weight: ", terminator: "")
+var userWeight = readLine()!
+
+print("Now enter your height: ", terminator: "")
+var userHeight = readLine()!
+
+
+print("Your BMI is: " + String(format: "%.2f", calculateBMI(weight: (Double(userWeight)!), height: (Double(userHeight)!))))
