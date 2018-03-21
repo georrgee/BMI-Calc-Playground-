2396e383e07e520ce9ede7defde4bc0b108521df
George Garcia <geor@vicki-2.local>
2018-03-20 21:07:48 -0700

    Initial Commit

---
+++ BMI Calc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BMI Calc
+//
+//  Created by George Garcia on 3/20/18.
+//  Copyright © 2018 GTeam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
